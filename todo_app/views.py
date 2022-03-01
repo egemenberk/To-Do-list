3fa5e677164efa8ce5ca34b094264d79e46c2daf
Egemen Galatali <egemen.galatali@plentific.com>
2022-03-01 21:59:41 +0300

    Return objects

--- todo_app/views.py
+++ todo_app/views.py
@@ -8,13 +8,11 @@ todo_list_schema = TodoListSchema()
 
 
 def create_todo(todo, user):
-    todo_list_id = todo.get("todo_list_id")
+    todo_list_id = todo.pop("todo_list_id")
     todo_list = TodoList.query.filter(TodoList.id == todo_list_id).one_or_none()
     if not todo_list:
         abort(404, f"TodoList item with id: {todo_list_id} is not found")
-    new_todo = Todo(**todo)
-    db.session.add(new_todo)
-    db.session.commit()
+    new_todo = Todo.create(todo_list, **todo)
     return todo_schema.jsonify(new_todo), 201
 
 
@@ -42,6 +40,7 @@ def create_user(user):
     new_user = User(**user)
     db.session.add(new_user)
     db.session.commit()
+    return new_user
 
 
 def create_todo_list(user):
