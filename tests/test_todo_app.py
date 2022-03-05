e7fb38d8d40c6ffe6c94040f130bba7e970101e4
Egemen Galatali <egemen.galatali@plentific.com>
2022-03-05 17:05:13 +0300

    Make endpoint names plural form

--- tests/test_todo_app.py
+++ tests/test_todo_app.py
@@ -9,7 +9,7 @@ def test_auth_fails_without_correct_username(test_client, user, _user):
     headers = {
         "Authorization": _basic_auth_str(**_user),
     }
-    response = test_client.get("todo", headers=headers)
+    response = test_client.get("todos", headers=headers)
     assert response.status_code == 401
 
 
@@ -18,58 +18,58 @@ def test_auth_fails_without_correct_password(test_client, user, _user):
     headers = {
         "Authorization": _basic_auth_str(**_user),
     }
-    response = test_client.get("todo", headers=headers)
+    response = test_client.get("todos", headers=headers)
     assert response.status_code == 401
 
 
 def test_create_todo_list(session, test_client, user):
-    response = test_client.post("todo-list")
+    response = test_client.post("todo-lists")
     assert response.status_code == 201
     assert TodoList.query.all()[0].id == response.json["id"]
 
 
 def test_create_todo(session, test_client, user, todo_list):
     response = test_client.post(
-        "todo", json={"todo_list_id": todo_list.id, "text": "text", "completed": False}
+        "todos", json={"todo_list_id": todo_list.id, "text": "text", "completed": False}
     )
     assert response.status_code == 201
     assert Todo.query.all()[0].id == response.json["id"]
 
 
 def test_get_todo_lists_for_user(session, test_client, user, todo_list):
-    response = test_client.get("todo-list")
+    response = test_client.get("todo-lists")
     assert response.status_code == 200
     assert todo_list.id == response.json[0]["id"]
 
 
 def test_get_todos_for_user(session, test_client, user, todo):
-    response = test_client.get("todo")
+    response = test_client.get("todos")
     assert response.status_code == 200
     assert todo.id == response.json[0]["id"]
 
 
 def test_complete_todo(session, test_client, user, todo):
     assert todo.completed != True
-    response = test_client.post(f"todo/{todo.id}/complete")
+    response = test_client.post(f"todos/{todo.id}/complete")
     assert response.status_code == 204
     assert todo.completed == True
 
 
 def test_delete_todo(session, test_client, user, todo):
     assert len(Todo.query.all()) > 0
-    response = test_client.delete(f"todo/{todo.id}")
+    response = test_client.delete(f"todos/{todo.id}")
     assert response.status_code == 204
     assert len(Todo.query.all()) == 0
 
 
 def test_delete_todo(session, test_client, user, todo):
     assert len(Todo.query.all()) > 0
-    response = test_client.delete(f"todo/{todo.id}")
+    response = test_client.delete(f"todos/{todo.id}")
     assert response.status_code == 204
     assert len(Todo.query.all()) == 0
 
 
 def test_delete_not_existing_todo(session, test_client, user, todo):
     assert len(Todo.query.all()) > 0
-    response = test_client.delete(f"todo/{99999}")
+    response = test_client.delete(f"todos/{99999}")
     assert response.status_code == 404
